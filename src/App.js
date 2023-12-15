aa8a7298e9b3967c254c20190571ef179ef644f2
Cameron Mikel <97004239+CamMikel91@users.noreply.github.com>
2023-12-15 16:48:10 -0700

    react-taskManager displays stored tasks from file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import tasks from "./fakeTaskService-1";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center">Task Manager</h1>
+      <ul className="list-unstyled">
+        {tasks.map((task) => (
+          <li key={task._id}>
+            <strong>Task ID</strong>: {task._id} <br />
+            <strong>Title:</strong> {task.title} <br />
+            <strong>Task:</strong> {task.task} <br />
+            <strong>Category:</strong> {task.category} <br />
+            <strong>Severity:</strong> {task.severity.name} <br />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
